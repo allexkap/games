cb7d0e2676f0a2f3ed5f56d9b00560205e2162e9
allexkap <aekpstn@gmail.com>
2022-10-24 19:34:30 +0300

    add auto mode (maze)

---
+++ maze_auto.py
@@ -0,0 +1,25 @@
+from os import get_terminal_size
+from time import sleep
+from maze import Player
+
+
+def Bot(player, delay=0.1):
+    DIRS = ('up', 'left', 'down', 'right')
+    ANGLES = (1, -1, -1, -1)
+    failed = 0
+    current = 0
+    while True:
+        if player.move(DIRS[current]):
+            failed += 1
+        else:
+            failed = 0
+            sleep(delay)
+        current = (current+ANGLES[failed])%4
+
+x, y = get_terminal_size()
+player = Player((x-10)//4, (y-5)//2)
+
+try:
+    Bot(player, 0.05)
+except:
+    print(f'\33[{2*player.m+5};{0}H')
